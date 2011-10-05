9b2d539571ea7468a490b9763d36c17dd807ffb4
Mikhail Vorozhtsov <mikhail.vorozhtsov@gmail.com>
2011-10-05 22:16:22 +0700

    Fixed printf format string.

--- include/posix-timer.macros.h
+++ include/posix-timer.macros.h
@@ -2,7 +2,7 @@
 #define POSIX_TIMER_MACROS_H
 
 #define hsc_itype(t) \
-  printf("%s%i", ((t)(-1)) < 0 ? "Int" : "Word", sizeof (t) * 8);
+  printf("%s%lu", ((t)(-1)) < 0 ? "Int" : "Word", sizeof (t) * 8);
 
 #endif /* POSIX_TIMER_MACROS_H */
 
